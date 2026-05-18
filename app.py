cf084725eefb116a767c3fb52763da2f704107f4
Alice13Yu <alisay@campus.technion.ac.il>
2026-05-18 14:10:34 +0300

    Creating a GitHub Repo + Project Skeleton

---
+++ app.py
@@ -0,0 +1,6 @@
+# TODO: replace with real content in lectures 4-5
+import streamlit as st
+ 
+st.set_page_config(page_title="SHADY", layout="wide")
+st.title("SHADY")
+st.write("Coming soon - EDA dashboard + ML model")
